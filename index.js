a666c42aabc62c73294cdca817c53ca3a5d4f646
Rahinur Miah <miahrahinur2002@gmail.com>
2025-07-09 14:49:01 -0400

    hello

--- index.js
+++ index.js
@@ -7,22 +7,42 @@ document.addEventListener("DOMContentLoaded", function () {
   const previousbtn = document.querySelector(".previous-button");
 
   const songs = [
+     {
+       title: "Starship - Nothing's Gonna Stop Us Now",
+       author: "Starship",
+       url: "sounds/NothingGoingToStopUsNow.mp3",
+     },
     {
-      title: "Nothings going to stop us now",
-      author: "starship",
-      url: "sounds/NothingGoingToStopUsNow.mp3"
+      title:
+        "SHAUN feat. Conor Maynard - Way Back Home (Lyrics) Sam Feldt Edit",
+      author: "SHAUN feat. Conor Maynard",
+      url: "sounds/waybackhome.mp3",
     },
 
   ];
 
   let currentSong = 0;
+  
   let audio = new Audio(songs[currentSong].url);
 
   songTitle.textContent = songs[currentSong].title;
   songAuthor.textContent = songs[currentSong].author;
 
-  playbtn.addEventListener("click", function() {
+  playbtn.addEventListener("click", function () {
     audio.play();
-  })
+  });
+
+  nextbtn.addEventListener("click", function () {
+    audio.pause();
+    audio.currentTime = 0;
+    currentSong = (currentSong + 1) % songs.length;
+    audio = new Audio(songs[currentSong].url);
+    audio.play();
+    songTitle.textContent = songs[currentSong].title;
+    songAuthor.textContent = songs[currentSong].author;
+
+  });
+
+  
 
 });
